c4cffcd4076ddafddb352362c0649cf9abea8529
arun777akj <75570747+arun777akj@users.noreply.github.com>
2024-07-09 22:23:01 -0400

    Update features.ts

--- src/components/Features/features.ts
+++ src/components/Features/features.ts
@@ -7,26 +7,26 @@ import { WheelChairIcon } from "../icons/WheelChairIcon";
 
 export const features = [
   {
-    name: "Prelims test series and Intensive Mains Answer Writing Practice",
+    name: "Collection of Children's Stories",
     description:
-      "Includes detailed analysis & feedback. It will include practice tests for the examination, Weekly module wise class tests and full-length test series before the examination",
+      "Old favorite children's stories translated into French/English and read slowly by a native French/English speaker.",
     icon: ChartIcon,
   },
   {
     name: "Helpful Mentorship Circles",
     description:
-      "Classroom coaching is enriched with individual care and during the course, guidance from mentors will be available for doubt clearance, answer writing corrections and prelims and mains strategy preparation",
+      "Classroom coaching is enriched with individual care and during the course, guidance from mentors will be available for doubt clearance, answer writing corrections and exam strategy preparation",
     icon: TeamIcon,
   },
   {
     name: "Expert faculty",
     description:
-      "Classes covering the entire syllabus are handled by experienced teachers in the field of competitive exams",
+      "Classes covering the entire syllabus are handled by experienced teachers in the field of examination",
     icon: ExpertIcon,
   },
   {
     name: "Comprehensive and exhaustive study materials",
-    description: "These are always updated and relevant to KAS examination",
+    description: "These are always updated and relevant to respective examination",
     icon: StudyMaterial,
   },
   {
