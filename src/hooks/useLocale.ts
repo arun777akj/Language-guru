2f548fc806e1f51bbe1f8110aefed7cdec9e2b5d
arun777akj <75570747+arun777akj@users.noreply.github.com>
2024-07-17 12:46:00 -0400

    edited ml to fr

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,13 +1,13 @@
 import { useRouter } from "next/router";
 import { en } from "../locales/en";
-import { ml } from "../locales/ml";
+import { fr } from "../locales/ml";
 
 export const useLocale = (props: string[]): { [key: string]: string } => {
   const { locale } = useRouter();
 
   if (locale)
     return props.reduce((a, v) => {
-      const val = locale === "en" ? en[v] : ml[v];
+      const val = locale === "en" ? en[v] : fr[v];
 
       if (val) return { ...a, [v]: val };
       return { ...a, [v]: v };
